5613610e3d7ff6af1263fddbe7867f9f1923bdf7
Sathishjain12 <159761455+Sathishjain12@users.noreply.github.com>
2024-02-29 18:49:02 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            By Sathish{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
